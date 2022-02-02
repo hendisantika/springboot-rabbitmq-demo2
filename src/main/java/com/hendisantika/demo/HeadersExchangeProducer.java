57e4b4ecb7e279a6794200325f0ab7821030ebfc
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-02 19:02:32 +0700

    #31 - Add sendMsg in  HeadersExchangeProducer

--- src/main/java/com/hendisantika/demo/HeadersExchangeProducer.java
+++ src/main/java/com/hendisantika/demo/HeadersExchangeProducer.java
@@ -1,9 +1,17 @@
 package com.hendisantika.demo;
 
+import com.hendisantika.model.OrderTaxiMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.messaging.converter.MessageConverter;
+import org.springframework.messaging.converter.SimpleMessageConverter;
 import org.springframework.stereotype.Service;
+
+import static com.hendisantika.config.RabbitMqHeadersExchangeConfig.EXCHANGE_TAXI_HEADERS;
+import static com.hendisantika.util.MessagingLoggingUtil.logSendMessage;
 
 /**
  * Created by IntelliJ IDEA.
@@ -26,5 +34,19 @@ public class HeadersExchangeProducer {
         sendMsg("eco.large", 1);
         sendMsg("eco.large", 2);
         sendMsg("eco.large", 3);
+    }
+
+    private void sendMsg(Object headerFrom, Object headerPricingModel) {
+        OrderTaxiMessage orderTaxiMessage = new OrderTaxiMessage();
+
+        MessageProperties messageProperties = new MessageProperties();
+        messageProperties.setHeader("from", headerFrom);
+        messageProperties.setHeader("pricingModel", headerPricingModel);
+
+        MessageConverter messageConverter = new SimpleMessageConverter();
+        Message message = messageConverter.toMessage(orderTaxiMessage, messageProperties);
+
+        rabbitTemplate.convertAndSend(EXCHANGE_TAXI_HEADERS, "", message);
+        logSendMessage(EXCHANGE_TAXI_HEADERS, "", orderTaxiMessage, messageProperties.getHeaders());
     }
 }
