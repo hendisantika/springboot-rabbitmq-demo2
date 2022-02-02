b0ac11aae63c13c292b355f5970906c5955dedfa
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-02 18:54:19 +0700

    #16 - Add directExchangeBindings in RabbitMqDirectExchangeConfig

--- src/main/java/com/hendisantika/config/RabbitMqDirectExchangeConfig.java
+++ src/main/java/com/hendisantika/config/RabbitMqDirectExchangeConfig.java
@@ -1,6 +1,9 @@
 package com.hendisantika.config;
 
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Declarables;
 import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -25,5 +28,20 @@ public class RabbitMqDirectExchangeConfig {
     @Bean
     public DirectExchange exchangeTaxiDirect() {
         return new DirectExchange(EXCHANGE_TAXI_DIRECT);
+    }
+
+    @Bean
+    public Declarables directExchangeBindings(
+            DirectExchange exchangeTaxiDirect,
+            Queue queueTaxiNormalSmall,
+            Queue queueTaxiEcoSmall,
+            Queue queueTaxiNormalLarge,
+            Queue queueTaxiEcoLarge) {
+        return new Declarables(
+                BindingBuilder.bind(queueTaxiNormalSmall).to(exchangeTaxiDirect).with(ROUTING_KEY_TAXI_NORMAL_SMALL),
+                BindingBuilder.bind(queueTaxiEcoSmall).to(exchangeTaxiDirect).with(ROUTING_KEY_TAXI_ECO_SMALL),
+                BindingBuilder.bind(queueTaxiNormalLarge).to(exchangeTaxiDirect).with(ROUTING_KEY_TAXI_NORMAL_LARGE),
+                BindingBuilder.bind(queueTaxiEcoLarge).to(exchangeTaxiDirect).with(ROUTING_KEY_TAXI_ECO_LARGE)
+        );
     }
 }
