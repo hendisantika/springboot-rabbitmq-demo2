b86d266f8845f26396f92632f3de7abe0a3f7696
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-02 19:01:02 +0700

    #29 - Add HeadersExchangeProducer

---
+++ src/main/java/com/hendisantika/demo/HeadersExchangeProducer.java
@@ -0,0 +1,23 @@
+package com.hendisantika.demo;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-rabbitmq-demo2
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 02/02/22
+ * Time: 19.00
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class HeadersExchangeProducer {
+    private final RabbitTemplate rabbitTemplate;
+}
