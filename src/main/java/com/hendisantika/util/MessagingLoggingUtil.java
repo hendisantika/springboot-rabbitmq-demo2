1dd69d0e27673c55ac919cbef652949056d5a9a5
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-02 18:48:59 +0700

    #4 - Add MessagingLoggingUtil

---
+++ src/main/java/com/hendisantika/util/MessagingLoggingUtil.java
@@ -0,0 +1,19 @@
+package com.hendisantika.util;
+
+import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-rabbitmq-demo2
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 02/02/22
+ * Time: 18.48
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+@UtilityClass
+public class MessagingLoggingUtil {
+}
