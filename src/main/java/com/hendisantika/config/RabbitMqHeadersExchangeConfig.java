b19f7b77936049e835babfe7f0007d47725e2976
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-02 18:56:11 +0700

    #20 - Add RabbitMqHeadersExchangeConfig

---
+++ src/main/java/com/hendisantika/config/RabbitMqHeadersExchangeConfig.java
@@ -0,0 +1,18 @@
+package com.hendisantika.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-rabbitmq-demo2
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 02/02/22
+ * Time: 18.55
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class RabbitMqHeadersExchangeConfig {
+    public static final String EXCHANGE_TAXI_HEADERS = "x.taxi.headers";
+}
