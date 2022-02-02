18ae8b27ff9fe7d2c84c73102896f3dff963e365
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-02 18:56:47 +0700

    #22 - Add headersExchangeBindings in RabbitMqHeadersExchangeConfig

--- src/main/java/com/hendisantika/config/RabbitMqHeadersExchangeConfig.java
+++ src/main/java/com/hendisantika/config/RabbitMqHeadersExchangeConfig.java
@@ -1,8 +1,13 @@
 package com.hendisantika.config;
 
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Declarables;
 import org.springframework.amqp.core.HeadersExchange;
+import org.springframework.amqp.core.Queue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,5 +26,16 @@ public class RabbitMqHeadersExchangeConfig {
     @Bean
     public HeadersExchange exchangeTaxiHeaders() {
         return new HeadersExchange(EXCHANGE_TAXI_HEADERS);
+    }
+
+    @Bean
+    public Declarables headersExchangeBindings(
+            HeadersExchange exchangeTaxiHeaders,
+            Queue queueTaxiNormalSmall,
+            Queue queueTaxiEcoLarge) {
+        return new Declarables(
+                BindingBuilder.bind(queueTaxiNormalSmall).to(exchangeTaxiHeaders).whereAny(Map.of("from", "eco.small", "pricingModel", 1)).match(),
+                BindingBuilder.bind(queueTaxiEcoLarge).to(exchangeTaxiHeaders).whereAll(Map.of("from", "eco.large", "pricingModel", 2)).match()
+        );
     }
 }
