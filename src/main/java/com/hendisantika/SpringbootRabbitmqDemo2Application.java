fe3d1f3602ed57edb69635d97d2629c31000f48c
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-02 19:07:14 +0700

    #41 - Add CommandLineRunner in Main App

--- src/main/java/com/hendisantika/SpringbootRabbitmqDemo2Application.java
+++ src/main/java/com/hendisantika/SpringbootRabbitmqDemo2Application.java
@@ -1,13 +1,30 @@
 package com.hendisantika;
 
+import com.hendisantika.demo.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootRabbitmqDemo2Application {
+@RequiredArgsConstructor
+public class SpringbootRabbitmqDemo2Application implements CommandLineRunner {
+    private final DefaultExchangeProducer defaultExchangeProducer;
+    private final DirectExchangeProducer directExchangeProducer;
+    private final FanoutExchangeProducer fanoutExchangeProducer;
+    private final TopicExchangeProducer topicExchangeProducer;
+    private final HeadersExchangeProducer headersExchangeProducer;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootRabbitmqDemo2Application.class, args);
     }
 
+    @Override
+    public void run(String... args) {
+        defaultExchangeProducer.sendMessage();
+        directExchangeProducer.sendMessage();
+        fanoutExchangeProducer.sendMessage();
+        topicExchangeProducer.sendMessage();
+        headersExchangeProducer.sendMessage();
+    }
 }
