9ba510419789ad4298f31e00e4eec61cf97c9490
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-02 19:03:44 +0700

    #33 - Add listenOnQueueDefault in TaxiQueuesListener

--- src/main/java/com/hendisantika/demo/TaxiQueuesListener.java
+++ src/main/java/com/hendisantika/demo/TaxiQueuesListener.java
@@ -1,7 +1,12 @@
 package com.hendisantika.demo;
 
+import com.hendisantika.model.OrderTaxiMessage;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
+
+import static com.hendisantika.config.QueuesConfiguration.QUEUE_TAXI_DEFAULT;
+import static com.hendisantika.util.MessagingLoggingUtil.logReceivedMessage;
 
 /**
  * Created by IntelliJ IDEA.
@@ -16,4 +21,10 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 public class TaxiQueuesListener {
+
+    @RabbitListener(queues = {QUEUE_TAXI_DEFAULT})
+    public void listenOnQueueDefault(OrderTaxiMessage message) {
+        logReceivedMessage(QUEUE_TAXI_DEFAULT, message);
+    }
+
 }
