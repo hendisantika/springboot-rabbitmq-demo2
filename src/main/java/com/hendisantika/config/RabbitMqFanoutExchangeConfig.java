af497bd8304323fa8c0da3ebd11d1ab11ee4ede2
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-02 18:55:42 +0700

    #19 - Add fanoutExchangeBindings in RabbitMqFanoutExchangeConfig

--- src/main/java/com/hendisantika/config/RabbitMqFanoutExchangeConfig.java
+++ src/main/java/com/hendisantika/config/RabbitMqFanoutExchangeConfig.java
@@ -1,6 +1,9 @@
 package com.hendisantika.config;
 
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Declarables;
 import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.Queue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,5 +24,20 @@ public class RabbitMqFanoutExchangeConfig {
     @Bean
     public FanoutExchange exchangeTaxiFanout() {
         return new FanoutExchange(EXCHANGE_TAXI_FANOUT);
+    }
+
+    @Bean
+    public Declarables fanoutExchangeBindings(
+            FanoutExchange exchangeTaxiFanout,
+            Queue queueTaxiNormalSmall,
+            Queue queueTaxiEcoSmall,
+            Queue queueTaxiNormalLarge,
+            Queue queueTaxiEcoLarge) {
+        return new Declarables(
+                BindingBuilder.bind(queueTaxiNormalSmall).to(exchangeTaxiFanout),
+                BindingBuilder.bind(queueTaxiEcoSmall).to(exchangeTaxiFanout),
+                BindingBuilder.bind(queueTaxiNormalLarge).to(exchangeTaxiFanout),
+                BindingBuilder.bind(queueTaxiEcoLarge).to(exchangeTaxiFanout)
+        );
     }
 }
