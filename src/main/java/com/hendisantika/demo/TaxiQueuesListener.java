bf85875f022acbb902db72fefaa90dc226765703
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-02 19:03:13 +0700

    #32 - Add TaxiQueuesListener

---
+++ src/main/java/com/hendisantika/demo/TaxiQueuesListener.java
@@ -0,0 +1,19 @@
+package com.hendisantika.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-rabbitmq-demo2
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 02/02/22
+ * Time: 19.02
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+@Service
+public class TaxiQueuesListener {
+}
