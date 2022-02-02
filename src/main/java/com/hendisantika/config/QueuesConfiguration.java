c96aa58223ae317ac6b4aded445ec73bea913a51
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-02 18:50:37 +0700

    #8 - Add QueuesConfiguration

---
+++ src/main/java/com/hendisantika/config/QueuesConfiguration.java
@@ -0,0 +1,22 @@
+package com.hendisantika.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-rabbitmq-demo2
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 02/02/22
+ * Time: 18.50
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class QueuesConfiguration {
+    public static final String QUEUE_TAXI_DEFAULT = "q.taxi.default";
+    public static final String QUEUE_TAXI_NORMAL_SMALL = "q.taxi.normal.small";
+    public static final String QUEUE_TAXI_ECO_SMALL = "q.taxi.eco.small";
+    public static final String QUEUE_TAXI_NORMAL_LARGE = "q.taxi.normal.large";
+    public static final String QUEUE_TAXI_ECO_LARGE = "q.taxi.eco.large";
+}
