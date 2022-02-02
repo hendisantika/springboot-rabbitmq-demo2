9bda2d5db6cc194bbdd14f8709e7b311e66b0a1e
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-02 19:06:23 +0700

    #40 - Add sendMsg in TopicExchangeProducer

--- src/main/java/com/hendisantika/demo/TopicExchangeProducer.java
+++ src/main/java/com/hendisantika/demo/TopicExchangeProducer.java
@@ -1,9 +1,13 @@
 package com.hendisantika.demo;
 
+import com.hendisantika.model.OrderTaxiMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
+
+import static com.hendisantika.config.RabbitMqTopicExchangeConfig.EXCHANGE_TAXI_TOPIC;
+import static com.hendisantika.util.MessagingLoggingUtil.logSendMessage;
 
 /**
  * Created by IntelliJ IDEA.
@@ -25,5 +29,11 @@ public class TopicExchangeProducer {
         sendMsg("taxi.normal.small");
         sendMsg("whatever.any.large");
         sendMsg("taxi.eco.additional.words");
+    }
+
+    private void sendMsg(String routingKey) {
+        OrderTaxiMessage message = new OrderTaxiMessage();
+        rabbitTemplate.convertAndSend(EXCHANGE_TAXI_TOPIC, routingKey, message);
+        logSendMessage(EXCHANGE_TAXI_TOPIC, routingKey, message);
     }
 }
