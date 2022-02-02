cb3f3bcea3f61d7ed9fd1473d8b5d691151dbf05
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-02 18:45:38 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/SpringbootRabbitmqDemo2Application.java
@@ -0,0 +1,13 @@
+package com.hendisantika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRabbitmqDemo2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRabbitmqDemo2Application.class, args);
+    }
+
+}
