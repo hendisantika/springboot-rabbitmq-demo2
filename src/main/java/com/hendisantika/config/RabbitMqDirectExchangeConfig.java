8620f6b5367d304148e32dce722b2c5a23a9a4d3
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-02 18:53:25 +0700

    #134 - Add RabbitMqDirectExchangeConfig

---
+++ src/main/java/com/hendisantika/config/RabbitMqDirectExchangeConfig.java
@@ -0,0 +1,22 @@
+package com.hendisantika.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-rabbitmq-demo2
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 02/02/22
+ * Time: 18.53
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class RabbitMqDirectExchangeConfig {
+    public static final String EXCHANGE_TAXI_DIRECT = "x.taxi.direct";
+    public static final String ROUTING_KEY_TAXI_NORMAL_SMALL = "taxi.normal.small";
+    public static final String ROUTING_KEY_TAXI_ECO_SMALL = "taxi.eco.small";
+    public static final String ROUTING_KEY_TAXI_NORMAL_LARGE = "taxi.normal.large";
+    public static final String ROUTING_KEY_TAXI_ECO_LARGE = "taxi.eco.large";
+}
