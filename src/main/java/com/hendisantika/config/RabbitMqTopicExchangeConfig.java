b00ad4c309a6177bd63ccb3741bf666691c5f439
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-02 18:57:20 +0700

    #23 - Add RabbitMqTopicExchangeConfig

---
+++ src/main/java/com/hendisantika/config/RabbitMqTopicExchangeConfig.java
@@ -0,0 +1,18 @@
+package com.hendisantika.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-rabbitmq-demo2
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 02/02/22
+ * Time: 18.57
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class RabbitMqTopicExchangeConfig {
+    public static final String EXCHANGE_TAXI_TOPIC = "x.taxi.topic";
+}
