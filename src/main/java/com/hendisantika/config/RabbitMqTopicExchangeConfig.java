3ed0eea7933084b2b7d0d27e0babebdd3053249f
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-02 18:58:28 +0700

    #25 - Add topicExchangeBindings in RabbitMqTopicExchangeConfig

--- src/main/java/com/hendisantika/config/RabbitMqTopicExchangeConfig.java
+++ src/main/java/com/hendisantika/config/RabbitMqTopicExchangeConfig.java
@@ -1,8 +1,13 @@
 package com.hendisantika.config;
 
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Declarables;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import static com.hendisantika.config.RabbitMqDirectExchangeConfig.*;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,4 +27,24 @@ public class RabbitMqTopicExchangeConfig {
     public TopicExchange exchangeTaxiTopic() {
         return new TopicExchange(EXCHANGE_TAXI_TOPIC);
     }
+
+    @Bean
+    public Declarables topicExchangeBindings(
+            TopicExchange exchangeTaxiTopic,
+            Queue queueTaxiNormalSmall,
+            Queue queueTaxiEcoSmall,
+            Queue queueTaxiNormalLarge,
+            Queue queueTaxiEcoLarge) {
+        return new Declarables(
+                BindingBuilder.bind(queueTaxiNormalSmall).to(exchangeTaxiTopic).with(ROUTING_KEY_TAXI_NORMAL_SMALL),
+                BindingBuilder.bind(queueTaxiEcoSmall).to(exchangeTaxiTopic).with(ROUTING_KEY_TAXI_ECO_SMALL),
+                BindingBuilder.bind(queueTaxiNormalLarge).to(exchangeTaxiTopic).with(ROUTING_KEY_TAXI_NORMAL_LARGE),
+                BindingBuilder.bind(queueTaxiEcoLarge).to(exchangeTaxiTopic).with(ROUTING_KEY_TAXI_ECO_LARGE),
+                BindingBuilder.bind(queueTaxiNormalLarge).to(exchangeTaxiTopic).with("*.*.large"),
+                BindingBuilder.bind(queueTaxiEcoLarge).to(exchangeTaxiTopic).with("*.*.large"),
+                BindingBuilder.bind(queueTaxiEcoSmall).to(exchangeTaxiTopic).with("taxi.eco.#"),
+                BindingBuilder.bind(queueTaxiEcoLarge).to(exchangeTaxiTopic).with("taxi.eco.#")
+        );
+    }
+
 }
